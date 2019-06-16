e66c168112fcaaa9b52b76efc36781b763f6278d
Softwarica College Lab <softwarica@softwarica.com>
2019-06-16 10:56:19 +0545

    Initial commit

---
+++ app/src/main/java/com/services/MainActivity.java
@@ -0,0 +1,42 @@
+package com.services;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import service.MyService;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMyService();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMyService();
+            }
+        });
+    }
+
+    private void startMyService(){
+        startService(new Intent(this, MyService.class));
+    }
+    private void stopMyService(){
+        stopService(new Intent(this, MyService.class));
+    }
+}
